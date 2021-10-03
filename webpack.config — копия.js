724a1e19803e9127a295277f5c3bd379011bb30a
Stranger <rooderuler@gmail.com>
2021-10-03 20:45:21 +0300

    use Webpack && babel

---
+++ webpack.config — копия.js
@@ -0,0 +1,66 @@
+module.exports = {
+  mode: "development",
+
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+
+      // Loading images
+      {
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }
+        ]
+      },
+
+      // Loading fonts
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+
+      //Loading CSS
+      {
+        test: /\.(css)$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
+      },
+
+      //Loading SASS/SCSS
+      {
+        test: /\.(s[ca]ss)$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'},
+        ]
+      }
+
+    ]
+  }
+};
